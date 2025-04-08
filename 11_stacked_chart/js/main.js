62bce88711c7ee346ddf67c6f292df76582360c5
DanielRPrado <ed.prado0607@gmail.com>
2025-04-08 04:20:38 -0600

    Screenshots and modifications

--- 11_stacked_chart/js/main.js
+++ 11_stacked_chart/js/main.js
@@ -31,9 +31,14 @@ var yAxisCall = d3.axisLeft().tickFormat(formatBillion);
 
 // Area generator
 // TODO: create the area generator. 
+var area = d3.area()
+        .x(d => x(d.data.date))
+        .y0(d => y(d[0]))
+        .y1(d => y(d[1]));
 // The x coordinate will be the date of the data
 // while y0 and y1 will be in the 0 and 1 positions of the d element
 // TODO: create the stack
+var stack = d3.stack();
 
 // Axis groups
 var xAxis = g.append("g")
@@ -64,7 +69,7 @@ d3.csv('data/stacked_area2.csv').then((data) => {
     }));
         
     //TODO: obtain the keys array, remember to remove the first column
-    var keys = [];
+    var keys = d3.keys(data[0]).filter(key => key !== 'date');
 
 	data.forEach((d) => {
 	    d.date = parseDate(d.date);
@@ -87,14 +92,40 @@ d3.csv('data/stacked_area2.csv').then((data) => {
     // Add stacked area chart
     // TODO: finish the configuration of the stack object
     // by setting the keys, order and offset
+    stack.keys(keys)
+        .order(d3.stackOrderNone)
+        .offset(d3.stackOffsetNone);
 
     // TODO: bind the data to the stack and create the group 
     // that will contain the area path
+    var stackData = stack(data);
 
     // TODO: call the area generator with the appropriate data
+    var layers = g.selectAll(".layer")
+        .data(stackData)
+        .enter().append("g")
+        .attr("class", "layer")
+        .attr("fill", d => color(d.key));
+
+    layers.append("path")
+        .attr("d", area);
 
     // Create legend
     // TODO: Create a legend showing all the names of every color
+    legend.selectAll("rect")
+        .data(keys)
+        .enter().append("rect")
+        .attr("y", (d, i) => i * 20)
+        .attr("width", 18)
+        .attr("height", 18)
+        .attr("fill", color);
+
+    legend.selectAll("text")
+        .data(keys)
+        .enter().append("text")
+        .attr("y", (d, i) => i * 20 + 9)
+        .attr("x", 24)
+        .text((d) => d);
 
 }).catch((error) => {
     console.log(error);
